73474fa8e80135643bfa534f075b9fee6a1ab4c3
Jack McEllin <jack.mcellin@ul.ie>
2023-01-28 21:31:54 +0000

    Add root CMakeLists and Grace's main header file

---
+++ grace.hpp
@@ -0,0 +1,37 @@
+#ifndef _GRACE_HPP_
+#define _GRACE_HPP_
+
+// Abstract classes
+#include "./abstract/Arguments.hpp"
+#include "./abstract/Crossover.hpp"
+#include "./abstract/Evaluator.hpp"
+#include "./abstract/EvolutionaryAlgorithm.hpp"
+#include "./abstract/Genome.hpp"
+#include "./abstract/Initialiser.hpp"
+#include "./abstract/Mapper.hpp"
+#include "./abstract/Mutation.hpp"
+#include "./abstract/Population.hpp"
+#include "./abstract/RNG.hpp"
+#include "./abstract/Replacement.hpp"
+#include "./abstract/Selection.hpp"
+#include "./abstract/Settings.hpp"
+#include "./abstract/Statistics.hpp"
+#include "./abstract/Termination.hpp"
+
+// Algorithms
+#include "./algorithm/GeneticAlgorithm.hpp"
+
+// Genomes
+#include "./genome/GEGenome.hpp"
+
+// Operator classes
+#include "./operators/crossover/GECrossover.hpp"
+#include "./operators/initialiser/GEInitialiser.hpp"
+#include "./operators/mapper/GEMapper.hpp"
+#include "./operators/mutation/GEMutation.hpp"
+#include "./operators/termination/GETermination.hpp"
+
+// Utility classes
+#include "util/DerivationTree.hpp"
+
+#endif
